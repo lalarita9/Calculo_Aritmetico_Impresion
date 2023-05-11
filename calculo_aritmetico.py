5711e0b7088da406a901655e4387df4933c07eb2
Carolina Lara <lalarita@hotmail.com>
2023-05-11 17:55:11 -0400

    Primer commit

---
+++ calculo_aritmetico.py
@@ -0,0 +1,11 @@
+#Requerimos imprimir en pantalla el resultado de la multiplicación de dos variables, 
+# las cuales llamaremos “a y b”, y les asignaremos los valores 20 y 30, respectivamente. 
+# Debemos documentar cada paso de la operación haciendo uso de comentarios.
+
+#Se crea las variables a y b
+a = 20
+b = 30
+#Se crea la operación multiplicar de los 2 números dados
+multiplicar = a * b 
+#Se usa la función print istruyendo que imprima el resultado de la multiplicación
+print(multiplicar)
